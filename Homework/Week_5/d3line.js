f7a0dca870e6802132c6ce9791cc7ad5cfbd912f
davidvschie <31818074+davidvschie@users.noreply.github.com>
2018-03-02 19:04:09 +0100

    preparation for week 5

---
+++ Homework/Week_5/d3line.js
@@ -0,0 +1,22 @@
+// David van Schie, 10800999
+
+httpGetAsync("https://raw.githubusercontent.com/davidvschie/data_processing/master/Homework/Week_5/data.json",function(data){
+	var parsed = JSON.parse(data);
+
+	for (i in parsed) {
+		console.log(parsed[i])
+	}
+})
+
+// Code for asynchronous HTTP get request taken from 
+// https://stackoverflow.com/questions/247483/http-get-request-in-javascript
+function httpGetAsync(URL, callback)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", URL, true);
+    xmlHttp.send(null);
+}
